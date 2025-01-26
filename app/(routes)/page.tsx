57527c97ce41345ea066875cb62eb579ce531618
bponce <oroboruo566@gmail.com>
2025-01-26 10:17:14 -0500

    feat: add responsive menu

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,21 +1,47 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs"
 import { CardSummary } from "./CardSummary"
-import { UserRound } from "lucide-react"
+import { BookOpenCheck, UserRound, UsersRound, Waypoints } from "lucide-react"
+
+const dataCardsSummary = [
+  {
+    icon: UsersRound,
+    total: "12.450",
+    average: 15,
+    title: "Companies created",
+    tooltipText: "See all of the companies created"
+  },
+  {
+    icon: Waypoints,
+    total: "86.5%",
+    average: 80,
+    title: "Total Revenue",
+    tooltipText: "See all of the summary"
+  },
+  {
+    icon: BookOpenCheck,
+    total: "363,95€",
+    average: 30,
+    title: "Bounce Rate",
+    tooltipText: "See all of the bounce rate"
+  },
+]
 
 export default function Home() {
   return (
     <div>
-      <UserButton />
       <h2 className="text-3xl mb-4">Dashboar</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <CardSummary
-          icon={UserRound}
-          total="12.648"
-          average={15}
-          title="Company created"
-          tooltipText="This is the tooltip text"
-        />
+        {dataCardsSummary.map(({ icon, total, average, title, tooltipText }) => (
+          <CardSummary
+            key={title}
+            icon={icon}
+            total={total}
+            average={average}
+            title={title}
+            tooltipText={tooltipText}
+          />
+        ))}
       </div>
     </div>
   )
--- components/CustomTooltip/CustomTooltip.tsx
+++ components/CustomTooltip/CustomTooltip.tsx
@@ -1,8 +1,25 @@
+import { Info } from "lucide-react";
 import { CustomTooltipProps } from "./CustomTooltip.types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export function CustomTooltip(props: CustomTooltipProps) {
   const { content } = props;
   return (
-    <div>CustomTooltip</div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Info strokeWidth={1} className="h-5 w-5" />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{content}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+
   )
 }
